c862a81c53bd1807f533d854a649b740cab77492
Ivan Giubilei <ivan@Air-di-Ivan.homenet.telecomitalia.it>
2023-10-05 17:22:04 +0200

    added graphical distances

--- knn/knn.py
+++ knn/knn.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import ast
 from collections import Counter
 
-points = {'blue': [[1, 0, 0], [1, 0, 1], [1, 1, 0]],
-          'red': [[0, 0, 1], [0, 1, 0], [0, 1, 1]]}
+points = {'1': [[1, 0, 0], [1, 0, 1], [1, 1, 0]],
+          '2': [[0, 0, 1], [0, 1, 0], [0, 1, 1]]}
 
 new_point = [0, 1, 0.5]
 
@@ -27,23 +29,37 @@ class KNN:
         categories = [category[1] for category in sorted(distances)[:self.k]]
         result = Counter(categories).most_common(1)[0][0]
         return result
-    
+
+# help page
+if len(sys.argv) > 1 and ("--help" in sys.argv or "-h" in sys.argv):
+    print("Usage:\n\tpython knn.py\n\tuse -sl or --show-lines to show the distances")
+    sys.exit(0)
+
+# actual usage of knn
 clf = KNN()
 clf.fit(points)
 print("the point is predicted to be: " + clf.predict(new_point))
 
-# Creating figure
+# Plotting
 fig = plt.figure(figsize = (10, 7))
 ax = plt.axes(projection ="3d")
 
-for point in points["blue"]:
+for point in points["1"]:
     ax.scatter(point[0], point[1], point[2], color="#104DCA", s=60)
 
-for point in points["red"]:
+for point in points["2"]:
     ax.scatter(point[0], point[1], point[2], color="#FF0000", s=60)
     
 new_class = clf.predict(new_point)
-color = "#FF0000" if new_class == "red" else "#104DCA"
+color = "#FF0000" if new_class == "2" else "#104DCA"
 
+# drawing lines from every point to the classified one
+if len(sys.argv) > 1 and ("--show-lines" in sys.argv or "-sl" in sys.argv):
+    for point in points["1"]:
+        ax.plot([new_point[0], point[0]], [new_point[1], point[1]], [new_point[2], point[2]], color="#ADD8E6")
+    for point in points["2"]:
+        ax.plot([new_point[0], point[0]], [new_point[1], point[1]], [new_point[2], point[2]], color="#ffb3c1")
+
+# scatter plot of predicted point
 ax.scatter3D(new_point[0], new_point[1], new_point[2], marker="*", s=200, zorder=100, color=color)
 plt.show()
